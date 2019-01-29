99516970ca95592f1f71fff0130987d6c699630f
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-01-29 23:32:01 +0100

    804

--- src/UniqueMorseCodeWords804.java
+++ src/UniqueMorseCodeWords804.java
@@ -1,23 +1,32 @@
-//import java.util.Scanner;
-//
-//
-//
-//public class UniqueMorseCodeWords804 {
-//
-//    public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//
-//        String str = scan.nextLine();
-//
-//        String result = uniqueMorseRepresentations(words);
-//        System.out.println(result);
-//
-//
-//
-//    public static int uniqueMorseRepresentations(String[] words) {
-//        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-//        String[] alphabet = {"a", "b", "c"};
-//        alphabet.
-//        }
-//    }
-//}
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class UniqueMorseCodeWords804 {
+
+    private static final String[] MORSE_ENCODING = new String[] {
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+            "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    public static void main(String[] args) {
+
+
+        String[] words = {"gin", "zen", "gig", "msg"};
+
+        System.out.println(uniqueMorseRepresentations(words));
+
+    }
+
+    public static int uniqueMorseRepresentations(String[] words) {
+        Set<String> transformations = new HashSet<String>();
+        for (String word : words) {
+            final StringBuilder transformation = new StringBuilder();
+            for(char character : word.toCharArray()){
+                transformation.append(MORSE_ENCODING[character - 'a']);
+            }
+         transformations.add(transformation.toString());
+        }
+        return transformations.size();
+    }
+}
