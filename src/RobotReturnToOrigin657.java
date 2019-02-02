06c77584250714bbbf7469b0b58685408296c6f6
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-02 19:05:54 +0100

    657

---
+++ src/RobotReturnToOrigin657.java
@@ -0,0 +1,34 @@
+public class RobotReturnToOrigin657 {
+
+    public static void main(String[] args) {
+
+
+        String input = "UDLR";
+
+
+    }
+
+
+    public  boolean judgeCircle(String moves) {
+        int countHorizontal = 0;
+        int countVertical = 0;
+        for (int i = 0; i < moves.length(); i++){
+        if (moves.charAt(i) == 'U'){
+            countHorizontal++;
+        }
+        if (moves.charAt(i) == 'D'){
+            countHorizontal--;
+        }
+            if (moves.charAt(i) == 'L'){
+                countVertical++;
+            }
+            if (moves.charAt(i) == 'R'){
+                countVertical--;
+            }
+    }
+    if (countHorizontal == 0 && countVertical == 0){
+            return true;
+    }
+    return false;
+}
+}
