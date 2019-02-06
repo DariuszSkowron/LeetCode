7cc8788e8ea0744980a4943f58c1d7c88f49f7f2
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-06 21:48:20 +0100

    561

--- src/ArrayPartitionI561.java
+++ src/ArrayPartitionI561.java
@@ -20,3 +20,17 @@ public class ArrayPartitionI561 {
         return sum;
     }
 }
+
+
+//Shorter solution, you dont need to checking the minumum value beacuse
+// its already in order so lower position means lower digit
+//    public static int arrayPairSum(int[] nums) {
+//        Arrays.sort(nums);
+//        int sum = 0;
+//        for (int i = 0; i < nums.length; i += 2){
+//            sum += nums[i];
+//        }
+//
+//        return sum;
+//    }
+}
