59aa8ccf1887a9f4ae897afbeb3074e040217863
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-09 19:44:48 +0100

    852

---
+++ src/PeakIndexInAMountainArray852.java
@@ -0,0 +1,17 @@
+public class PeakIndexInAMountainArray852 {
+
+    public static void main(String[] args) {
+        int[] A = {0,1,0};
+
+        System.out.println(peakIndexInMountainArray(A));
+    }
+
+    public static int peakIndexInMountainArray(int[] A) {
+            for (int i = 1; i < A.length -1; i++) {
+                if (A[i] > A[i - 1] && A[i] > A[i + 1]) {
+                    return i;
+                };
+            }
+            return -1;
+    }
+}
