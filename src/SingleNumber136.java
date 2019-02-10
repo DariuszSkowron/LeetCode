4163cfcb4344fef8e45c288c14fd1d4696062d8c
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-10 21:59:01 +0100

    136

---
+++ src/SingleNumber136.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class SingleNumber136 {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 1, 2, 1, 2};
+
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
+        }
+        return result;
+    }
+}
