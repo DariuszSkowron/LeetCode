bb90bc7355fbbae3c0f4a09e9c640dec620036c4
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-10 13:32:02 +0100

    509

---
+++ src/FibbonaciNumber509.java
@@ -0,0 +1,14 @@
+public class FibbonaciNumber509 {
+
+    public static void main(String[] args) {
+
+        int N = 40;
+
+        System.out.println(fib(N));
+    }
+
+    public static int fib(int N) {
+        if(N <= 1) return N;
+        return fib(N-1)+fib(N-2);
+    }
+}
