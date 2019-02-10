0d95e463ca9098b723098218eae22739334f7b8e
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-10 17:18:53 +0100

    589

--- src/NAryTreePreorderTraversal589.java
+++ src/NAryTreePreorderTraversal589.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class NAryTreePostorderTraversal590 {
+public class NAryTreePreorderTraversal589 {
 
 
     List<Integer> list = new ArrayList<>();
 
-    public List<Integer> postorder(Node root) {
+    public List<Integer> preorder(Node root) {
         if (root == null) return list;
-        for (Node node : root.children) postorder(node);
-
         list.add(root.val);
+        for (Node node : root.children) preorder(node);
 
         return list;
     }
@@ -28,4 +28,3 @@ public class NAryTreePostorderTraversal590 {
         }
     }
 }
-
