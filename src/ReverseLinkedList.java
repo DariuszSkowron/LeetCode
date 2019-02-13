630ad85e42ad89ceb27081b8e47cb816cfa603ac
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-13 23:16:34 +0100

    206

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,43 @@
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+
+    public static ListNode reverseListRecursive(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode reversedListHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return reversedListHead;
+    }
+
+}
+
+
+
