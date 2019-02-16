4e6088070da71a6ca282611d59b5b6faa58ae272
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-16 22:31:07 +0100

    70

--- src/ClimbingStairs70.java
+++ src/ClimbingStairs70.java
@@ -1,22 +1,19 @@
 public class ClimbingStairs70 {
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 9;
 
         System.out.println(climbStairs(n));
     }
 
     public static int climbStairs(int n) {
-        // base cases
-        if(n <= 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        if (n <= 0 || n == 1 || n == 2) return n;
 
         int oneStepBefore = 2;
         int twoStepsBefore = 1;
         int allWays = 0;
 
-        for(int i=2; i<n; i++){
+        for (int i = 2; i < n; i++) {
             allWays = oneStepBefore + twoStepsBefore;
             twoStepsBefore = oneStepBefore;
             oneStepBefore = allWays;
