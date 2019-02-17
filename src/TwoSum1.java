274827b4b3a8888523cc6d389c24cf7af2a0570f
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-17 23:08:17 +0100

    1

---
+++ src/TwoSum1.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum1 {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])){
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
+
+
+//    int[] response = sortedArray.stream().mapToInt(i->i).toArray()
