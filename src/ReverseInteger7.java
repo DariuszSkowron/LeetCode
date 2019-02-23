7dad0449f7e1639244c175e06428d59e70e47a5a
Dariusz <dariusz.skowron@gmail.com>
2019-02-23 15:20:31 +0100

    7

--- src/ReverseInteger7.java
+++ src/ReverseInteger7.java
@@ -3,25 +3,26 @@ import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
 public class ReverseInteger7 {
 
     public static void main(String[] args) {
-        int x = -120;
+        Integer x = -2143847412;
 
         System.out.println(reverse(x));
 
     }
 
-    public static String reverse(int x) {
-        String transformation = Integer.toString(x);
-        StringBuilder result = new StringBuilder();
-        for (int i = transformation.length() -1 ; i >= 0; i--) {
-            if (transformation.substring(0,1).equals("-")) {
-                result.append('-');
-                i--;
-            }
-            result.append(transformation.charAt(i));
+    public static int reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
         }
-        if (result.charAt(0) == '0') result.deleteCharAt(0);
 
-        return result.toString();
-
+        return result;
     }
 }
