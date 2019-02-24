628e8a2ed50bbee4a1cd376bf4fbfad4e96c4cb5
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-24 22:39:20 +0100

    217

---
+++ src/ContainsDuplicate217.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate217 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 0};
+
+        System.out.println(containsDuplicate(nums));
+    }
+
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+}
+
+
+//
+//    public static boolean containsDuplicate(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//        for (int num : nums) {
+//            if (set.contains(num)) return true;
+//            set.add(num);
+//        }
+//        return false;
+//
+//    }
+//}
