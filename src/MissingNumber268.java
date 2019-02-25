6138d8a88766779d4222f31da5398d0e51848c0e
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-25 22:02:16 +0100

    268

---
+++ src/MissingNumber268.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MissingNumber268 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+
+        System.out.println(missingNumber(nums));
+
+    }
+
+    public static int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[nums.length-1] != nums.length) {
+            return nums.length;
+        }
+        else if (nums[0] != 0) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length -1; i++){
+            if (nums[i] + 1 != nums[i +1]) return nums[i] +1;
+        }
+        return -1;
+    }
+}
