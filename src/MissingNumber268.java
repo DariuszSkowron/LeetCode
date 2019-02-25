fe3e6071d5595f42a2092eb75ff047e38722580c
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-25 22:10:53 +0100

    268

--- src/MissingNumber268.java
+++ src/MissingNumber268.java
@@ -4,23 +4,33 @@ public class MissingNumber268 {
 
     public static void main(String[] args) {
 
-        int[] nums = {9,6,4,2,3,5,7,0,1};
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
 
         System.out.println(missingNumber(nums));
 
     }
 
-    public static int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[nums.length-1] != nums.length) {
-            return nums.length;
-        }
-        else if (nums[0] != 0) {
-            return 0;
-        }
-        for (int i = 0; i < nums.length -1; i++){
-            if (nums[i] + 1 != nums[i +1]) return nums[i] +1;
-        }
-        return -1;
+
+    public static int missingNumber(int[] nums) { //sum
+        int len = nums.length;
+        int sum = (0 + len) * (len + 1) / 2;
+        for (int i = 0; i < len; i++)
+            sum -= nums[i];
+        return sum;
     }
 }
+
+//    public static int missingNumber(int[] nums) {
+//        Arrays.sort(nums);
+//        if (nums[nums.length-1] != nums.length) {
+//            return nums.length;
+//        }
+//        else if (nums[0] != 0) {
+//            return 0;
+//        }
+//        for (int i = 0; i < nums.length -1; i++){
+//            if (nums[i] + 1 != nums[i +1]) return nums[i] +1;
+//        }
+//        return -1;
+//    }
+//}
