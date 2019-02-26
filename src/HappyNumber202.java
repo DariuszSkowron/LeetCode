86b6ffeb3d8efa0b5e5ace40de1e38de79887a31
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-26 20:49:01 +0100

    202

---
+++ src/HappyNumber202.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber202 {
+
+    public static void main(String[] args) {
+        int n = 19;
+
+        System.out.println(isHappy(n));
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> inLoop = new HashSet<Integer>();
+        int squareSum, remain;
+        while (inLoop.add(n)) {
+            squareSum = 0;
+            while (n > 0) {
+                remain = n % 10;
+                squareSum += remain * remain;
+                n /= 10;
+            }
+            if (squareSum == 1)
+                return true;
+            else
+                n = squareSum;
+
+        }
+        return false;
+
+    }
+}
