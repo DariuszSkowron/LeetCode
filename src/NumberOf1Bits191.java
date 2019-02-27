0ac43df47445b84921c14cd13bc7a7302365791c
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-02-27 22:11:44 +0100

    191

--- src/NumberOf1Bits191.java
+++ src/NumberOf1Bits191.java
@@ -6,13 +6,30 @@ public class NumberOf1Bits191 {
         System.out.println(hammingWeight(n));
     }
 
-    public static int hammingWeight(int n) {
-        String transformation = Integer.toBinaryString(n);
-        int count = 0;
-        for (int i = 0; i < transformation.length(); i++) {
-            if (transformation.charAt(i) == '1') count++;
-        }
-        return count;
 
+    public static int hammingWeight(int n) {
+        int ones = 0;
+        while (n != 0) {
+            ones = ones + (n & 1);
+            n = n >>> 1;
+        }
+        return ones;
     }
 }
+//    public static int hammingWeight(int n) {
+//        String transformation = Integer.toBinaryString(n);
+//        int count = 0;
+//        for (int i = 0; i < transformation.length(); i++) {
+//            if (transformation.charAt(i) == '1') count++;
+//        }
+//        return count;
+//
+//    }
+//}
+
+
+//    public static int hammingWeight(int n) {
+//        return Integer.bitCount(n);
+//
+//    }
+//}
