e371e34bdc4c348d2d1b0471e0670fe75a9eb7c2
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-03 22:38:52 +0100

    28

--- src/ImplementstrStr28.java
+++ src/ImplementstrStr28.java
@@ -12,11 +12,22 @@ public class ImplementstrStr28 {
     public static int strStr(String haystack, String needle) {
         if (needle.length() == 0 && haystack.length() != 0) return 0;
         if (needle.length() != 0 && haystack.length() == 0) return -1;
-        else if (haystack.contains(needle)) {
-            return haystack.indexOf(needle);
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).contains(needle)) return i;
         }
         return -1;
 
     }
 }
+
+//    public static int strStr(String haystack, String needle) {
+//        if (needle.length() == 0 && haystack.length() != 0) return 0;
+//        if (needle.length() != 0 && haystack.length() == 0) return -1;
+//        else if (haystack.contains(needle)) {
+//            return haystack.indexOf(needle);
+//        }
+//        return -1;
+//
+//    }
+//}
 
