1944a11e115bdad7ba049029a561a858933e8338
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-04 22:14:02 +0100

    198

--- src/HouseRobber198.java
+++ src/HouseRobber198.java
@@ -8,12 +8,26 @@ public class HouseRobber198 {
 
 
     public static int rob(int[] nums) {
-        int prevNo = 0, prevYes = 0;
-        for (int n : nums) {
-            int temp = prevNo;
-            prevNo = Math.max(prevNo, prevYes);
-            prevYes = n + temp;
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++){
+            nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
         }
-        return Math.max(prevNo, prevYes);
+        return nums[nums.length -1];
     }
+
 }
+
+
+//    public static int rob(int[] nums) {
+//        int prevNo = 0, prevYes = 0;
+//        for (int n : nums) {
+//            int temp = prevNo;
+//            prevNo = Math.max(prevNo, prevYes);
+//            prevYes = n + temp;
+//        }
+//        return Math.max(prevNo, prevYes);
+//    }
+//}
