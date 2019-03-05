ab9398a9fb78e9ae14c9179bbcfb547bf20a55ec
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-05 22:29:58 +0100

    88

--- src/MergeSortedArray88.java
+++ src/MergeSortedArray88.java
@@ -4,20 +4,33 @@ public class MergeSortedArray88 {
 
     public static void main(String[] args) {
 
-        int[] nums1 = {1, 2, 3, 0, 0, 0}, nums2 = {2, 5, 6};
+        int[] A = {1, 2, 3,3, 6, 8}, B = {2, 5, 6};
         int m = 3, n = 3;
 
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(merge(A,m,B,n)));
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        while (n != 0) {
-            for (int i = m; i < nums1.length; i++) {
-                nums1[i] = nums2[n - 1];
-                n--;
-            }
+    public static int[] merge(int A[], int m, int B[], int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while(i >= 0 && j >= 0) {
+            A[k--] = A[i] > B[j] ? A[i--] : B[j--];
         }
-        Arrays.sort(nums1);
+        while(j >= 0) {
+            A[k--] = B[j--];
+        }
+        return A;
     }
-
 }
+
+
+//    public static void merge(int[] A, int m, int[] B, int n) {
+//        while (n != 0) {
+//            for (int i = m; i < A.length; i++) {
+//                A[i] = B[n - 1];
+//                n--;
+//            }
+//        }
+//        Arrays.sort(A);
+//    }
+//
+//}
