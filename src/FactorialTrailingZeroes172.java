8f919901a7aef5b248d8dfbc0aef7aaef184176a
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-11 22:10:10 +0100

    26

---
+++ src/FactorialTrailingZeroes172.java
@@ -0,0 +1,15 @@
+public class FactorialTrailingZeroes172 {
+
+    public static void main(String[] args) {
+        int n = 30;
+
+
+        System.out.println(trailingZeroes(n));
+    }
+
+    public static int trailingZeroes(int n) {
+        int s = 0;
+        while (n > 4) s += (n /= 5 );
+        return s;
+    }
+}
