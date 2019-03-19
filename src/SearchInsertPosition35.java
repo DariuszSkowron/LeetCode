d73d1ab84184b74289074c9ef82c41f222e2ce6b
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-19 21:57:43 +0100

    35

--- src/SearchInsertPosition35.java
+++ src/SearchInsertPosition35.java
@@ -11,15 +11,9 @@ public class SearchInsertPosition35 {
 
 
     public static int searchInsert(int[] nums, int target) {
-        if (nums.length == 0) return 0;
-        if (nums[nums.length -1] < target) return nums.length;
         for (int i = 0; i < nums.length; i++){
-            if (nums[i] == target) return i;
+            if (nums[i] >= target) return i;
         }
-        Arrays.sort(nums);
-        for (int y = 0; y < nums.length; y++){
-            if (nums[y] > target) return y;
-        }
-        return 0;
+        return nums.length;
     }
 }
