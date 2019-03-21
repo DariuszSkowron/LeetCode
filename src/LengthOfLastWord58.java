c826c3bf03cff7db8eac32d50d4c430641c06c96
Dariusz Skowron <dariusz.skowron@gmail.com>
2019-03-21 21:04:58 +0100

    58

--- src/LengthOfLastWord58.java
+++ src/LengthOfLastWord58.java
@@ -17,4 +17,18 @@ public class LengthOfLastWord58 {
 }
 
 
+//class Solution {
+//    public int lengthOfLastWord(String s) {
+//        int lastLen = 0;
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            if (s.charAt(i) == ' ' && lastLen > 0) {
+//                return lastLen;
+//            } else if (s.charAt(i) != ' ') {
+//                lastLen++;
+//            }
+//        }
+//        return lastLen;
+//    }
+//}
+
 
